f49a7c4efa1c279956ce23958ae081266a98503e
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-08 08:53:34 +0530

    added routes and controllers

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -18,18 +20,23 @@ app.get('/', (req, res) => {
     );
 });
 
+app.use('/api/v1/products', (req, res) => {})
+
 // ?Products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 5000;
-
 const start = async () => {
     try {
         // connect DB
-        app.listen(port, console.log(`Server is listening to the port ${port}`))
+        await connectDB(process.env.MONGO_URI);
+        app.listen(
+            port,
+            console.log(`Server is listening to the port ${port}`)
+        );
     } catch (error) {
         console.log(error);
     }
-}
- start();
+};
+start();
