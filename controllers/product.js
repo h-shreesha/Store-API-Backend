a07e70904f7a87a2f4ab29cc2367cbd0cdbf5667
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-09 22:23:43 +0530

    updated

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,34 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing  async errors')
-    res.status(200).json({ msg: 'Product testing route' });
+    const search = 'a'
+    // throw new Error('testing  async errors')
+    const products = await Product.find({
+        name: {$regex: search, $options: 'i'}
+    });
+    res.status(200).json({ products, nbHits: products.length });
+    // res.status(200).json({ msg: 'Product testing route' });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' });
+    const { featured, company, name } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if(company){
+        queryObject.company = company
+    }
+
+    if(name){
+        queryObject.name =  {$regex: search, $options: 'i'}
+    }
+
+    const products = await Product.find(queryObject);
+    console.log(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
