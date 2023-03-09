6af4a3ac52dbbef65f8b2998a2683301c1bef98a
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-09 08:07:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors')
 
 //async errors
 
@@ -6,6 +7,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/product');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -20,13 +22,13 @@ app.get('/', (req, res) => {
     );
 });
 
-app.use('/api/v1/products', (req, res) => {})
+app.use('/api/v1/products', productsRouter);
 
 // ?Products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         // connect DB
